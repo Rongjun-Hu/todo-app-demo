9d673bfc8e57d34f98d15b8e7dfcc8c08581ba8b
RongjunHu <rongjun.hu@student.uts.edu.au>
2021-01-11 17:41:48 +1100

    Create Login & Sign Up components.

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="blue">
+            <v-toolbar-title>
+              Login Form
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-text-field
+              prepend-icon="person"
+              name="email"
+              label="Email"
+              type="email"
+            >
+            </v-text-field>
+            <v-text-field
+              prepend-icon="lock"
+              name="password"
+              label="Password"
+              type="password"
+            >
+            </v-text-field>
+          </v-card-text>
+          <v-divider light></v-divider>
+          <v-card-actions>
+            <v-btn rounded color="indigo" dark>
+              Sign In
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn rounded color="primary" dark>
+              Login
+              <v-icon>keyboard_arrow_right</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style></style>
---
+++ src/components/Auth/Signup.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="blue">
+            <v-toolbar-title>
+              Sign Up Form
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-alert :value="true" color="error" icon="warning">
+                This user already exists, try a different set of data.
+              </v-alert>
+
+              <v-text-field
+                prepend-icon="person"
+                name="username"
+                label="Username"
+                :rules="[rules.required]"
+              >
+              </v-text-field>
+
+              <v-text-field
+                prepend-icon="email"
+                name="email"
+                label="Email"
+                :rules="[rules.required, rules.email]"
+              >
+              </v-text-field>
+
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+                v-model="password"
+                :rules="[rules.required]"
+              >
+              </v-text-field>
+
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Confirm Password"
+                type="password"
+                v-model="confirmPassword"
+                :rules="[rules.required]"
+                :error="!valid()"
+              >
+              </v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-divider light></v-divider>
+
+          <v-card-actions>
+            <v-btn rounded color="black" dark>Sign In</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn rounded color="success">
+              Register
+              <v-icon>keyboard_arrow_up</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    password: "",
+    confirmPassword: "",
+    rules: {
+      required: (value) => !!value || "Required",
+      email: (value) => {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return pattern.test(value) || "Invalid e-mail.";
+      },
+    },
+  }),
+  methods: {
+    valid() {
+      return this.password === this.confirmPassword;
+    },
+  },
+};
+</script>
+
+<style></style>
